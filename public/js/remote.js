5ab8a25c1079dd9396db611983e7d02f420b45d8
ZShadow01 <zshadow.sniper01@gmail.com>
2023-02-19 00:02:07 +0100

    Update light states when button is pressed

--- public/js/remote.js
+++ public/js/remote.js
@@ -2,7 +2,10 @@ function initRemote(remoteId, socket) {
     const remote = document.getElementById(remoteId);
     const btn1 = remote.querySelector('button');
     btn1.addEventListener('click', () => {
-        if (currentLightButton === null) return;
-        socket.emit('light-toggle', currentLightButton.value);
+        if (currentLightButton === null) {
+            socket.emit('lights');
+        } else {
+            socket.emit('light-toggle', currentLightButton.value);
+        }
     });
 }
